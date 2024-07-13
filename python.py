41c3ba00abe65e9ac8397f0cb73d8bc4c10c90ce
GR Tanzim <elitetox225@gmail.com>
2024-07-13 11:49:38 +0600

    Update python.py

--- python.py
+++ python.py
@@ -1,1 +1,3 @@
 list = ["a" , "b" , "c" , "d" , "e" , "f" , "g" , "h" , "i" , "k" , "l" , "m" , "n" , "o" , "p" , "q" , "r" , "s" , "t" , "u" , "v" , "w" , "x" , "y" , "z"]
+print(list[0] + 1)
+print(list[0] + [1])
