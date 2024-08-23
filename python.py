7d16f644a052a7092dc4dd58cf7d789dedba2be2
GR Tanzim <elitetox225@gmail.com>
2024-08-23 16:55:36 +0600

    Delete python.py

--- python.py
+++
@@ -1,3 +0,0 @@
-list = ["a" , "b" , "c" , "d" , "e" , "f" , "g" , "h" , "i" , "k" , "l" , "m" , "n" , "o" , "p" , "q" , "r" , "s" , "t" , "u" , "v" , "w" , "x" , "y" , "z"]
-print(list[0] + 1)
-print(list[0] + [1])
